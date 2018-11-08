a6d0ed6b0baa536444bf1b6e703324b9647b76e9
Eli Perez <eperez001@nyc.yearup.org>
2018-11-08 16:50:27 -0500

    catgifs

--- w03/d4/workshop/main.js
+++ w03/d4/workshop/main.js
@@ -1,3 +1,18 @@
 //This comes from data.js
 var gifs = window.data.data;
 
+console.log(gifs);
+
+function randomGifs(){
+  let gifsElement = document.getElementById('gifs');
+  if (gifsElement.style.display="none"){
+    gifsElement.style.display="block";
+  }
+  let randomNumber = Math.floor(Math.random() * gifs.length);
+gifsElement.src = gifs[randomNumber].images.original.url;
+}
+
+let button = document.querySelector('button');
+button.addEventListener('click', randomGifs);
+
+playGifs();
