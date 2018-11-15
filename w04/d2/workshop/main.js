e4880d0941fb9b2f2fb57b16dea7a7b83ac2d6c0
Eli Perez <eperez001@nyc.yearup.org>
2018-11-15 10:18:29 -0500

    first commit

--- w04/d2/workshop/main.js
+++ w04/d2/workshop/main.js
@@ -1,1 +1,18 @@
 console.log('🚀');
+ function display(){
+   axios.get("https://www.redbullshopus.com/products.json")
+   .then(something);
+
+   let number = [0, 1, 2, 3, 4, 5]
+   function something(response){
+     for(var i=0; i< number.length; i++){
+       let randNumber = number[i]
+       let product = document.querySelector('#images')
+       let img = response.data.products[2].images[2].src
+       product.src = img
+       console.log(product)
+     }
+   }
+ }
+ let button = document.querySelector('button')
+ button.addEventListener('click', display);
